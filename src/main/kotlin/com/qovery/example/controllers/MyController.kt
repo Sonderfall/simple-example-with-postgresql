7bd4e1f5c57388005ec25dd76307f5c215e1a301
Valentin <valentin.macheret.fr@gmail.com>
2020-03-23 13:50:23 +0100

    Add MyController

---
+++ src/main/kotlin/com/qovery/example/controllers/MyController.kt
@@ -0,0 +1,18 @@
+package com.qovery.example.controllers
+
+import com.qovery.example.repositories.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class MyController {
+
+    @GetMapping
+    fun poney() : Int {
+        return 42;
+    }
+
+}
